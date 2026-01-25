e5dc4a47445dcd537018c10a52a6a8a5384124e8
said-bot-star <herreravelazquezcesar2020@gmail.com>
2026-01-24 22:29:59 -0600

    Refactor Cube component for mouse interaction and geometry
    Updated Cube component to use mouse position for rotation and position. Changed geometry from sphere to box and adjusted size.

--- DYH-Render/playground/Cesar/assets/App.jsx
+++ DYH-Render/playground/Cesar/assets/App.jsx
@@ -1,18 +1,25 @@
 import { Canvas, useFrame } from "@react-three/fiber"
 import './App.css'
-import { useRef } from "react"
+import { useRef, useEffect } from "react"
+
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += delta
-    ref.current.rotation.z = Math.sin(state.clock.elapsedTime) * 2
+  useFrame((state) => {
+    if (!ref.current) return
+
+    ref.current.rotation.x = state.mouse.y * Math.PI
+    ref.current.rotation.y = state.mouse.x * Math.PI
+    ref.current.rotation.z = Math.sin(state.clock.elapsedTime) * 3.14159265358973284626433
     console.log(state.clock.elapsedTime)
+
+    ref.current.position.x = state.mouse.x * 3.14159265358973284626433
+    ref.current.position.y = state.mouse.y * 3.14159265358973284626433
   })
+
   return (
     <mesh position={position} ref={ref}>
-      <sphereGeometry arg={size} />
+      <boxGeometry arg={size} />
       <meshStandardMaterial color={color} />
     </mesh>
   )
@@ -23,7 +30,7 @@ const App = () => {
     <Canvas>
       <directionalLight position={(0, 0, .001)} />
       <ambientLight intensity={0.01} />
-      <Cube position={[-1, 0, 0]} color={"blue"} size={[1, 8, 1]} />
+      <Cube position={[0, 0, 0]} color={"blue"} size={[1, 50, 1]} />
     </Canvas>
   )
 }
