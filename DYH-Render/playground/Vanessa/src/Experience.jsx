e25ffc14e95755157b24f9ed5e4ff49896f79c72
Vanessa Rodriguez <vanessa.rdz.1d.35@gmail.com>
2026-02-07 19:10:55 -0600

    Vane Semana 4 Esferas felices que se mueven

--- DYH-Render/playground/Vanessa/src/Experience.jsx
+++ DYH-Render/playground/Vanessa/src/Experience.jsx
@@ -1,12 +1,31 @@
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei"
 import { Sphere } from "./Sphere"
+import { useAtom } from "jotai"
+// import { Players } from "./Players"
+import { playersAtom } from "./SocketManager"
+import { socket } from "./SocketManager"
 
 export const Experience = () => {
+
+    const [players]= useAtom(playersAtom)
+
     return (
         <>
-            <Sphere />
-            <OrbitControls enableZoom={false} />
-
+            <ambientLight />
+            {/* <Sphere /> */}
+            {/* <Players /> */}
+            <OrbitControls enableZoom={true} />
+            
+            { players.map((player)=> (
+                <Sphere 
+                    key={player.id}
+                    positionX={player.position[0]} 
+                    positionY={player.position[1]} 
+                    positionZ={player.position[2]}
+                    isLocal={player.id === socket.id} 
+                />
+            ))}
+            
             <ContactShadows
                 rotation-x={Math.PI /2}
                 position={[0, -1.6, 0]}
