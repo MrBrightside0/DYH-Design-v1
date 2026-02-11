2ed6789a399c1ae12d4dcb50b0fcd04072bf234e
said-bot-star <herreravelazquezcesar2020@gmail.com>
2026-02-10 21:17:52 -0600

    Refactor App component and integrate Cube component

--- DYH-Render/playground/Cesar/assets/App.jsx
+++ DYH-Render/playground/Cesar/assets/App.jsx
@@ -1,31 +1,30 @@
-import { Canvas, useFrame } from "@react-three/fiber"
-import './App.css'
-import { useRef, useEffect } from "react"
+import { Canvas } from "@react-three/fiber"
+import Cube from "./Cube"
 
+// Dejo apuntes para mi para saber q onda con esto jeje
 
-const Cube = ({ position, size, color }) => {
-  const ref = useRef()
-  useFrame((state) => {
-    if (!ref.current) return
+const App = () => {
 
-    ref.current.rotation.x = state.mouse.y * Math.PI
-    ref.current.rotation.y = state.mouse.x * Math.PI
-    ref.current.rotation.z = Math.sin(state.clock.elapsedTime) * 3.14159265358973284626433
-    console.log(state.clock.elapsedTime)
+  // este coso es el decide que rollito todo vrd
+  // true   es de q yo en yendo a la uni
+  // false  y esto ps yo en mi poderocicimo fime
 
-    ref.current.position.x = state.mouse.x * 3.14159265358973284626433
-    ref.current.position.y = state.mouse.y * 3.14159265358973284626433
-  })
+  const isSender = true
 
+  // a esto si le se, aqui tunie mi cubito y lo hice aesthetic
+  // el coso ese de abajo del 100 hace que aproveche toda la pantalla para q no quede chico pq el canvas es el coso q me fallaba
   return (
-    <mesh position={position} ref={ref}>
-      <boxGeometry arg={size} />
-      <meshStandardMaterial color={color} />
-    </mesh>
+    <Canvas style={{ width: "100ww", height: "100vh" }} camera={{ position: [0, 0, 5], fov: 69, near: 0.1, far: 1000 }}
+      gl={{ clearColor: "black" }}>
+      <camera position={[0, 0, 5]} />
+      <directionalLight position={(0, 0, 0.001)} />
+      <ambientLight intensity={0.01} />
+      <Cube isSender={isSender} />
+    </Canvas>
   )
 }
 
-const App = () => {
+export default App
   return (
     <Canvas>
       <directionalLight position={(0, 0, .001)} />
